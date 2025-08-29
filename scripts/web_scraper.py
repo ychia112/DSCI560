5220eab71c2bd97a2784d0028f5bdb55ec330237
ychia112 <yuchiacheng12@gmail.com>
2025-08-29 15:14:19 -0700

    fix: 2.3 finish

--- scripts/web_scraper.py
+++ scripts/web_scraper.py
@@ -1,10 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+
 url = "https://www.cnbc.com/world/?region=world"
-response = requests.get(url)
+response = requests.get(url, headers=headers)
 
 with open("../data/raw_data/web_data.html", "w", encoding="utf-8") as f:
     f.write(response.text)
 
 print("Done")
+
